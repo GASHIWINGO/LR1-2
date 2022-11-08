588de74478c0c6e0ebda04e1c27ec79c46b18186
GASHIWINGO <117731094+GASHIWINGO@users.noreply.github.com>
2022-11-08 14:13:15 +0300

    Add files via upload

---
+++ лр1-2/9.pas
@@ -0,0 +1,11 @@
+﻿program b33;
+var a,b,c,e:integer;
+begin
+  writeln('введите три числа');
+  readln(a,b,c);
+  e:=0;
+  if a>0 then inc(e);
+  if b>0 then inc(e);
+  if c>0 then inc(e);
+  writeln('количество положительных чисел=',e);
+end.
